306d85393f49daa3135f6e598eb13a9b188810f4
patileiti <pati.leiti@gmail.com>
2022-03-09 20:34:40 +0100

    fix event listeners, adjust #out styleing

--- src/wapp.ts
+++ src/wapp.ts
@@ -14,5 +14,5 @@ const digits = [   "[0]", "[1]", "[2]", "[3]", "[4]", "[5]", "[6]", "[7]", "[8]"
 
 let clock = new Clock((time) => out.innerText = time, digits)
 
-start.addEventListener("click", e => clock.start)
-stop.addEventListener("click", e => clock.stop)
+start.addEventListener("click", (e) => clock.start())
+stop.addEventListener("click", (e) => clock.stop())
